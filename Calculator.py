5677c2c518d35e812e7bece371cc1c56fc72dd02
Th3D4rk0rb <jimmydeathstalkerscorpion@gmail.com>
2026-07-15 11:59:52 +0700

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,5 @@
+print("Hey User, this program is a multiplying calculator")
+FirstFactor = int(input("Please input a number"))
+SecondFactor = int(input("Please input a second number"))
+Product = FirstFactor * SecondFactor
+print(f"The product of {FirstFactor} and {SecondFactor}, is {Product}")
