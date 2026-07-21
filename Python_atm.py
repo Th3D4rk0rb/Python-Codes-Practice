81ed404134ab3f68635223c6de6ce289da9f342b
Th3D4rk0rb <jimmydeathstalkerscorpion@gmail.com>
2026-07-21 11:48:59 +0700

    Update Python_atm.py
    This program is free :)

--- Python_atm.py
+++ Python_atm.py
@@ -4,8 +4,8 @@ if Password == 436875534533:
     print("Account Logged in")
     Balance = 100
     Withdraw = float(input("How much money do you want to withdraw?"))
-    if Withdraw <= 100:
-        NewBalance = (100 - Withdraw)
+    if Withdraw <= 144500:
+        NewBalance = (144500 - Withdraw)
         print(f"New Balance = {NewBalance}")
     else:
         print("Withdraw failed")
