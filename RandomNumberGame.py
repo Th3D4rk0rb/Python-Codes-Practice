0e5633a1061d21642413ad9cf7c1d2d96656e2b1
Th3D4rk0rb <jimmydeathstalkerscorpion@gmail.com>
2026-07-29 20:47:10 +0700

    Create RandomNumberGame.py
    This game is free. :)

---
+++ RandomNumberGame.py
@@ -0,0 +1,24 @@
+import random
+
+secret_number = random.randint(1, 10)
+attempts = 0
+
+print("I'm thinking of a number between 1 and 10.")
+
+while True:
+    try:
+        
+        guess = int(input("Take a guess: "))
+        attempts += 1  
+        
+        
+        if guess == secret_number:
+            print(f"🎉 Correct! You got it in {attempts} attempt{'s' if attempts > 1 else ''}.")
+            break
+        elif guess < secret_number:
+            print("Too low! Try again.")
+        else:
+            print("Too high! Try again.")
+            
+    except ValueError:
+        print("Please enter a valid whole number.")
